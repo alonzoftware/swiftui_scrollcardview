f9071505f446b7c37d3f09d5c138f42b24d018c9
MacMiniM1 <alonzo.choque@icloud.com>
2022-01-30 06:13:09 -0400

    Initial Commit

---
+++ ScrollCardView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollCardView
+//
+//  Created by Rene Alonzo Choque Saire on 30/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollCardView/ScrollCardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollCardViewApp.swift
+//  ScrollCardView
+//
+//  Created by Rene Alonzo Choque Saire on 30/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollCardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
